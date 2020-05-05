1a4e65e9607cb191cbeec9af5b25e089f48a7162
Jakub Pilimon <jakub.pilimon@gmail.com>
2020-05-05 13:53:28 +0200

    Biblioteka kliencka z fajnym API - Lights
    Biblioteka kliencka z fajnym API - Lights

---
+++ Lights.java
@@ -0,0 +1,15 @@
+public class Lights {
+
+    int position;
+
+    /**
+     * null - brak opcji w samochodzie
+     * 1-3 - w dół
+     * 7-10 - w górę
+     * @return
+     */
+    public Integer getLightsPosition() {
+        return position;
+    }
+
+}
